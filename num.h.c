65c6e6b6c4bc047ba27aef686d652f03ba728070
Maorgu24 <93335221+Maorgu24@users.noreply.github.com>
2021-11-08 21:42:48 +0200

    Add files via upload
    Final commit

---
+++ num.h.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void main(){
+    int x,y;
+    scanf("%i%i", &x,&y);
+
+
+
+    printf("Prime numbers are:");
+    // x<y
+    for(int i = x; i <= y; i++){
+        if(isPrime(i));
+        printf("%i"i);
+    }
+
+    printf("\nArmstrong numbers are:");
+    // x<y
+    for(int i = x; i <= y; i++){
+        if(isArmstrong(i));
+        printf("%i"i);
+    }    
+
+    printf("\nStrong numbers are:");
+    // x<y
+    for(int i = x; i <= y; i++){
+        if(isStrong(i));
+        printf("%i"i);
+    }
+    
+    printf("\nPalindrome numbers are:");
+    // x<y
+    for(int i = x; i <= y; i++){
+        if(isPalindrome(i));
+        printf("%i"i);
+    }
+    
+}
